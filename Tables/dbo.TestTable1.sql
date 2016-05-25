a4771bbd0bbc5fb03be9dc3b756e7e5e5dd6c17b
Dima Pachynin <dpachynin@revenuewell.com>
2016-05-25 16:43:45 -0500

    test not nullable column

--- Tables/dbo.TestTable1.sql
+++ Tables/dbo.TestTable1.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[TestTable1]
 (
 [Id] [int] NOT NULL,
 [Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[tt1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[tt1] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[testnull] [int] NOT NULL CONSTRAINT [DF__TestTable__testn__15502E78] DEFAULT ((0))
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[TestTable1] ADD CONSTRAINT [PK_TestTable1] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
