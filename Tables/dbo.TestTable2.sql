eeb58040b7da7070ad1edd44f62494d89f933a05
Dima Pachynin <dpachynin@revenuewell.com>
2016-05-25 13:34:39 -0500

    added xxx

--- Tables/dbo.TestTable2.sql
+++ Tables/dbo.TestTable2.sql
@@ -2,7 +2,8 @@ CREATE TABLE [dbo].[TestTable2]
 (
 [Id] [int] NOT NULL,
 [Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Age] [int] NULL
+[Age] [int] NULL,
+[xxx] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[TestTable2] ADD CONSTRAINT [PK_TestTable2] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
